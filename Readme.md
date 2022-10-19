ee02171ec325d4b5fdafcebb02965b96f095fe91
SuroZaqaryan <suren.zakaryan177@gmail.com>
2022-10-19 04:07:30 +0400

    initialize project

---
+++ Readme.md
@@ -0,0 +1,5 @@
+1. npm install
+
+2. npm run watch-css
+
+3. open index.html with live server
---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+$(function () {
+    $(".navbar-wrapper").load(`/src/components/navbar.html`);
+    $(".footer-wrapper").load(`/src/components/footer.html`);
+    $(".product-wrapper").load(`/src/pages/product.html`);
+});
